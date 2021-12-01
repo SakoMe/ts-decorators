dd9d4d7608006713644066eb9fd95d9bab2a2571
SakoMe <melkonsa@gmail.com>
2021-12-01 16:51:23 +0400

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+	res.send(
+		`<div>
+      <h1>Hello</h1>
+    </div>`
+	);
+});
+
+app.listen(3000, () => console.log('server up on 3000'));
